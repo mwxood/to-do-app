904424dd8caa6104095daea1da0ad6b2e237bc07
mwxood@gmail.com <mwxood@gmail.com>
2022-07-07 09:24:18 +0300

    added classs

--- src/components/card.js
+++ src/components/card.js
@@ -3,29 +3,28 @@ import { Container, Row, Col} from 'react-bootstrap';
 
 const Card = (props) => {
     const [active, setActive] = useState(false);
-    const currentEl = useRef(null);
 
     const activeElementHandler = (event) => {
         setActive(!active);
-        currentEl.current = event.target;
+
+        if(!event.target.classList.contains('active')) {
+            event.target.className += ' active'
+         } else {
+            event.target.className = 'col card col-md-6'
+         }
     }
 
-    useEffect(() => {
-        if(currentEl.current === null) {
-            return;
-        }
 
 
-        currentEl.current.className = active ? 'col card col-md-6 active' : 'col card col-md-6';
 
 
-    })
+   
 
     const cardItem = () => {
-        return props.cardItem.map(item => {
+        return props.cardItem.map((item, index) => {
             return (
                 <Row key={item.id} className='justify-content-center mt-5 mb-3'>
-                    <Col className='card col-md-6' onClick={activeElementHandler}>
+                    <Col className='col card col-md-6' onClick={activeElementHandler}>
                         {item.title}
                     </Col>
                 </Row>
